c875519943776e915e6e7ae59e314e776a702850
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 19:52:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,9 +78,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | season, holiday |
     puts "#{season.to_s.capitalize}:"
     holiday.each do | name, supply |
-      binding.pry
+      puts "#{holiday.keys.to_s.split("_").join(" ").capitalize}: "
 
-    # puts "#{holiday.to_s.split("_").join(" ").capitalize}: "
+
+    #
     end
   end
 end
