5b505109a39336d10e29fd5424668d3a89a0c26d
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 20:12:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do | season, holidays |
       holidays.select do |i|
         binding.pry
-        i.values.flatten.include?("BBQ")
+        holidays[i].values.flatten.include?("BBQ")
       end
     end
 end
